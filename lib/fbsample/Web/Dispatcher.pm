2c77292988ed9899e4c7c1747fb70fa9b0fa463e
hsksyusk <hsksyusk@gmail.com>
2012-02-19 17:20:40 +0900

    change to fbpage

--- lib/fbsample/Web/Dispatcher.pm
+++ lib/fbsample/Web/Dispatcher.pm
@@ -32,6 +32,26 @@ any '/' => sub {
 		}
 	);
 };
+
+any '/fbpage' => sub {
+    my ($c) = @_;
+	my $data;
+	my $token = $c->session->get('token');
+	if( $token ) { #loggedin
+		my $ua = LWP::UserAgent->new();
+		my $res = $ua->get("https://graph.facebook.com/me/home?access_token=${token}");
+		$res->is_success or die $res->status_line;
+		$data = decode_json($res->decoded_content);
+	}
+	$c->render(
+		'fbpage.tt',
+		{
+			name => $c->session->get('name'),
+			data => $data->{data},
+		}
+	);
+};
+
 =pod
 get '/fbpage' => sub {
 	my ($c) = @_;
